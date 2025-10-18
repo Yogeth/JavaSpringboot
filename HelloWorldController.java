707250057436d31996f681e86b4c8b1d58c14836
Yogeth <yogethcr@gmail.com>
2025-10-18 11:27:02 +0530

    update HelloWorldController.java

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -4,6 +4,6 @@
  public class HelloWorldController{
    @GetMapping("/world")
    String sayHello(){
-     return "Hello World !";
+     return "Hello World hi everyone iam Yogeth Rajendran from TamilNadu iam studying AiMl in Mahendra Institute of Technology !";
    }
  }

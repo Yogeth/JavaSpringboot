62a7eade5b2072f52b1800e481f8af409a437ea8
Yogeth <yogethcr@gmail.com>
2025-10-18 18:04:23 +0530

    update HelloWorldController.java

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -1,9 +1,14 @@
  package com.example.demo;
  import org.springframework.web.bind.annotation.*;
  @RestController
+ @RequestMapping("/hello")
  public class HelloWorldController{
    @GetMapping("/world")
    String sayHello(){
-     return "Hello World hi everyone i love java !";
+     return "Hello World i love java !";
+   }
+   @GetMapping("/everyone")
+   String sayHello(){
+     return "Hello everyOne iam yoge";
    }
  }

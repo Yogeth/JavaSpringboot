b7874eaf4695c9c1a2fc90e7e11045026a1b0bf7
Yogeth <yogethcr@gmail.com>
2025-10-19 07:36:55 +0530

    update

--- PathVariable_example.java
+++ PathVariable_example.java
@@ -2,7 +2,7 @@ package com.example.restservice;
  import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1")
-public class PathVariable{
+public class PathVariable_example{
  @GetMapping("/{id}")
   String getById(@PathVariable long id){
    return "example for path variable :"+id;

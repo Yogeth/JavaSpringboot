120b8ada45d96c861843e3640c5bc606fca88f5e
Yogeth <yogethcr@gmail.com>
2025-10-20 06:13:59 +0530

    update RequestParams.java

--- RequestParams.java
+++ RequestParams.java
@@ -1,9 +1,19 @@
 package com.example.restservice;
  import org.springframework.web.bind.annotation.*;
 @RestController
-class RequestParams{
+public class RequestParams{
  @GetMapping("/users")
- String getparam(@RequestParam long id ){
+/* String getparam(@RequestParam long id ){
   return "It is the id : "+ id;
+ }*/
+ 
+ String keys(
+  @RequestParam("usr_a") String userName , 
+ @RequestParam("pass_a") 
+ int passWord){
+  return "UserName = "+userName+"  "+"passWord = "+passWord;
  }
+ 
 }
+
+// in url localhost 8080/users ? usr_a&pass_a
